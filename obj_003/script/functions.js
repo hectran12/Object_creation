a0b33609190195667d9012c3c80540a5fc3d2bb0
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-07-26 14:58:52 +0700

    26/07/2022

---
+++ obj_003/script/functions.js
@@ -0,0 +1,164 @@
+function getValue (index, dea = true, content = "") {
+    const item = data[index];
+    const obj = item.objCreation[item.name];
+    const type = (item.type + "").toUpperCase();
+    let name, value;
+    func_name = () => {
+        eval('name = ' + prompt('Enter the name: '));
+    };
+    func_value = () => {
+        eval('value = ' + prompt('Enter the value: '));
+    };
+    func_custome = (content) => {
+        eval('value = ' + prompt('Enter the ' + content +': '));
+    }
+    if (dea) {
+        if (dea != "none") {
+            if (type == "SET" || type == "ARRAY") {
+                func_value();
+            } else {
+                func_name();
+                func_value();
+            }
+        }
+    } else {
+        func_custome(content);
+    }
+    
+    if (dea) {
+        return {
+            n: name, v: value,
+            o: obj, t: type
+        };
+    } else if (dea == "none") {
+        return {o: obj, t: type};
+    } else {
+        return {
+            v: value, o: obj, t: type,
+            mode: dea
+        }
+    }
+    
+}
+
+function add(index) {
+    try {
+        const item = getValue(index);
+        if (item.t == "SET") { item.o.add(item.v); }
+        showText("Action success");
+    } catch {
+        showText("Error");
+    }
+}
+
+function set(index) {
+    try {
+        const item = getValue(index);
+        if (item.t == "MAP") { item.o.set(item.n, item.v); }
+        showText("Action success");
+    } catch {
+        showText("Error");
+    }
+}
+
+function deletes (index) {
+    try {
+        const item = getValue(index, false, "key");
+        if (item.t == "SET" || item.t == "MAP") {
+            item.o.delete(item.v);
+        }
+        showText("Action success");
+    } catch {
+        showText("Error");
+    }
+}
+
+
+function has (index) {
+    try {
+        const item = getValue(index, false, "key");
+        if (item.t == "SET" || item.t == "MAP") {
+            if(item.o.has(item.v)) {
+                showText("Element is has (TRUE)");
+            } else {
+                showText("Element is has (FALSE)");
+            }
+        }
+    } catch {
+        showText("Error");
+    }
+}
+
+function clears  (index) {
+    try {
+        const item = getValue(index, "none");
+        if (item.t == "SET" || item.t == "MAP") {
+            item.o.clear();
+            console.log(item.o);
+            showText("View console");
+        }
+    } catch {
+        showText("Error");
+    }
+}
+
+function forEachs (index) {
+    try {
+        const item = getValue(index, false, "keywords ('value, index, orgin'): ");
+        if (item.t == 'SET' || item.t == 'MAP') {
+            content = `
+                item.o.forEach(function (${item.v}) {
+                    console.log(${item.v});
+                });
+            `;
+            console.log(content);
+            eval(content);
+        }
+        showText("View console");
+    } catch {
+        showText("Error");
+    }
+}
+
+function values (index) {
+    try {
+        const item = getValue(index, "none");
+        if(item.t == "SET" || item.t == "MAP") {
+            for (var element of item.o.values()) {
+                console.log(element);
+            }
+        }
+        showText("View console");
+    } catch {
+        showText("Error");
+    }
+}
+
+function keys (index) {
+    try {
+        const item = getValue(index, "none");
+        if (item.t == "SET" || item.t == "MAP") {
+            for (var element of item.o.keys()) {
+                console.log(element);
+            }
+        }
+        showText("View console");
+    } catch {
+        showText("Error");
+    }
+}
+
+
+function entries (index) {
+    try {
+        const item = getValue(index, "none");
+        if (item.t == "SET" || item.t == "MAP") {
+            for (var [key, value] of item.o.entries()) {
+                console.log(key, '=>', value);
+            }
+        }
+        showText("View console");
+    } catch {
+        showText("Error");
+    }
+}
