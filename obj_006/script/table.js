4e2d36f0a6a249312c01fb8d57d060effcfc6775
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-07-28 10:45:14 +0700

    28/07/2022

---
+++ obj_006/script/table.js
@@ -0,0 +1,58 @@
+let div = document.getElementById("listElement");
+
+function resetTable () {
+    div.innerHTML = "";
+    console.log("reset successful");    
+}
+
+function reloadTable (obj) {
+    resetTable();
+    var content = '';
+    let contentTitle, contentAction;
+    content += `<table>`;
+
+    contentTitle = ['Number', 'DateCreate', 'ObjectName', 'TypeOject', 'Action'];
+
+    contentAction = {
+        addElement: 'Add element',
+        removeElement: 'Remove Element',
+        functionElement: 'Function Element',
+        viewElement: 'View Element',
+        ExportCode: 'Export Code'
+    };
+
+    content += `<tr>`;
+    contentTitle.forEach(function (value, key) {
+        content += '<th>' + value + '</th>';
+        console.log(key, value);
+    });
+    content += `</tr>`;
+
+    // reverse obj to show newest element first
+    Object.keys(obj).reverse().forEach(function (key) {
+        const value = obj[key];
+        content += '<tr>';
+        content += '<td>' + key + '</td>';
+        content += '<td>' + value.dateCreation + '</td>';
+        content += '<td>' + value.name + '</td>';
+        content += '<td>' + value.type + '</td>';
+        content += '<td>';
+        for (let keyAction in contentAction) {
+
+            content +=`
+                <button onclick="${keyAction}(${value.id})">
+                    ${contentAction[keyAction]}
+                </button>
+            `;
+        }
+        content += '</td>';
+        content += `</tr>`;
+    });
+    
+    content += '</table>';
+    div.innerHTML = content;
+    return true;
+
+
+    
+}
